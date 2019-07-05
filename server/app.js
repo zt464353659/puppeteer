ce36e02ef88a30bf84cdd2528c2cadfbdcf3e9b4
yanwencheng <wcyan@suntekcorps.com>
2019-07-05 13:07:25 +0800

    登录接口

--- server/app.js
+++ server/app.js
@@ -8,10 +8,23 @@ const app = new koa()
 const main = async (ctx, next) => {
   ctx.response.type = 'html'
   ctx.response.body = await fs.readFile('./template.html', 'utf-8')
+  next()
+}
+const login = async (ctx, next) => {
+  const body = ctx.request.body
+  console.log(body.name)
+  console.log(body.password)
+  if (body.name && body.name === 'nice') {
+    if (body.password && body.password === 'nice') {
+      ctx.response.type = 'json'
+      ctx.response.body = { code: 200, msg: '登录成功！' }
+    }
+  }
 }
 app.use(cors())
-app.use(route.get('/', main))
 app.use(koaBody())
+app.use(route.get('/', main))
+app.use(route.post('/login', login))
 // app.use(cors())
 app.listen(3000, () => {
   console.log('server running ···')
