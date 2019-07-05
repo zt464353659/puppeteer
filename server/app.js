68b0fbd6f1f9c519908ff7bf31215da5875aaa78
yanwencheng <wcyan@suntekcorps.com>
2019-07-05 17:08:24 +0800

    添加首页数据接口

--- server/app.js
+++ server/app.js
@@ -12,19 +12,36 @@ const main = async (ctx, next) => {
 }
 const login = async (ctx, next) => {
   const body = ctx.request.body
-  console.log(body.name)
-  console.log(body.password)
+  // console.log(body.name)
+  // console.log(body.password)
   if (body.name && body.name === 'nice') {
     if (body.password && body.password === 'nice') {
       ctx.response.type = 'json'
-      ctx.response.body = { code: 200, msg: '登录成功！' }
+      ctx.response.body = { code: 200, msg: "登录成功！" }
     }
+  } else {
+    ctx.response.type = 'json'
+    ctx.response.body = { code: 20001, msg: '用户名或密码不能为空' }
+  }
+  next()
+}
+const index = async (ctx, next) => {
+  ctx.response.type = 'json'
+  ctx.response.body = {
+    list: [
+      { id: 1, name: 'tom', sex: 0, age: 12 },
+      { id: 2, name: 'alen', sex: 1, age: 13 },
+      { id: 3, name: 'martin', sex: 1, age: 12 },
+      { id: 4, name: 'alice', sex: 0, age: 11 }
+    ],
+    msg: '获取列表成功'
   }
 }
 app.use(cors())
 app.use(koaBody())
 app.use(route.get('/', main))
 app.use(route.post('/login', login))
+app.use(route.get('/index', index))
 // app.use(cors())
 app.listen(3000, () => {
   console.log('server running ···')
